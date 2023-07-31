2788578b2d7e92b0514ffcd265dfff6687a835ec
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2023-07-31 15:14:22 +0800

    Add files via upload

---
+++ js-b6sy8k/404.js
@@ -0,0 +1,14 @@
+var bodyBlue = document.getElementById('bodyBlue');
+bodyBlue.style.visibility = "visible";
+var i = 5;
+function redirect() {
+    document.getElementById('frmHeader').innerText = "Oops! Page not found. Redirecting (" + i + "s)"
+    if(i == 0) {
+        window.location.replace('https://creator.artcademy.ph/');
+    }
+    else {
+        i-=1;
+    }
+    
+}
+setInterval(redirect, 1000);
